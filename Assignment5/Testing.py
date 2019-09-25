0521a14b8034bee8abc61eed23e1d2bb3d165773
VortexCallouts <ftvortex1@gmail.com>
2019-09-24 22:56:33 -0500

    Added Python Testing Files

---
+++ Assignment5/Testing.py
@@ -0,0 +1,4 @@
+#Function One
+def Login(username,password):
+    if username and password == "user":
+        return True
---
+++ Assignment5/test_Test1.py
@@ -0,0 +1,5 @@
+import Testing
+
+def test_Login():
+    login = Testing.Login("user","user")
+    assert login == True
